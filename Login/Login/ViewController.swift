8b2362d046933cb0cbe353e2162b1834061dc22e
Mariam <mariam@Mariams-Mac.local>
2020-11-30 09:13:46 -0800

    Assignement 3 started

---
+++ Login/Login/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Mariam on 11/30/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
