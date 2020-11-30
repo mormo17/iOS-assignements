85fcf3e7299f937aec648ba4c9bebac106fe521c
Mariam <mariam@Mariams-Mac.local>
2020-11-30 14:09:50 -0800

    buttons done

--- Login/Login/ViewController.swift
+++ Login/Login/ViewController.swift
@@ -16,15 +16,50 @@ class ViewController: UIViewController {
     private var originalCenterY: NSLayoutConstraint!
     private var keyboardIsVisible = false
     
+    @IBOutlet weak var forgotPassword: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signupButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
         registerForKeyboardNotifications()
-        
+        customizeView()
         email.delegate = self
         password.delegate = self
      }
+    
+    private func customizeView(){
+        customizeTextFields()
+        customizeButtons()
+    }
+    
+    private func customizeTextFields(){
+        email.layer.cornerRadius = 5
+        password.layer.cornerRadius = 5
+    }
+    
+    private func customizeButtons(){
+        customizeForgotButton()
+        customizeLoginButton()
+        customizeSignupButton()
+    }
+    
+    private func customizeForgotButton(){
+        forgotPassword.contentHorizontalAlignment = .right
+    }
+    
+    private func customizeLoginButton(){
+        loginButton.backgroundColor = .blue
+        loginButton.layer.cornerRadius = 5
+        
+    }
+    
+    private func customizeSignupButton(){
+        signupButton.backgroundColor = .clear
+        signupButton.layer.cornerRadius = 5
+        signupButton.layer.borderWidth = 1
+        signupButton.layer.borderColor = UIColor.blue.cgColor
+    }
      
      override func viewWillDisappear(_ animated: Bool) {
        super.viewWillDisappear(true)
@@ -62,14 +97,14 @@ class ViewController: UIViewController {
     private func moveKeyboardUp(_ height: CGFloat){
         if keyboardIsVisible {return}
         originalCenterY = centerY
-        centerY.constant -= height
+        centerY.constant -= height * 0.75
         UIView.animate(withDuration: 1, delay: 0.0, usingSpringWithDamping: 0.3, initialSpringVelocity: 10.0, options: [], animations: {self.view.layoutIfNeeded()}, completion: nil)
         keyboardIsVisible = true
     }
     
     private func reserUI(){
         keyboardIsVisible = false
-        centerY.constant -= originalCenterY.constant
+        centerY.constant = 0
     }
 }
 
